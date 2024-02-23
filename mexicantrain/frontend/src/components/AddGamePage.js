650bd04c53c83debc519d43bea0430ef7c62a350
srh95 <srh95@case.edu>
2024-02-22 19:33:39 -0500

    Added table entry for game scores component

--- mexicantrain/frontend/src/components/AddGamePage.js
+++ mexicantrain/frontend/src/components/AddGamePage.js
@@ -2,41 +2,12 @@ import React, { useState, useEffect, useMemo } from "react";
 import axios from 'axios';
 import "/Users/sophiahall/Documents/TrenDB/mexicantrain/frontend/static/css/gameEntry.css";
 
-
 const AddGamePage = () => {
 
-    const [players, setPlayers] = useState([]); // state to store players
-    const [selectedPlayers, setSelectedPlayers] = useState([]); // state to store players which have been selected
-    const [rowCount, setRowCount] = useState(1); // state to store number of rows in the table
-    const [scores, setScores] = useState([]); // State to store scores
-
-
-    useEffect(() => {
-        fetchPlayers();
-    }, []);
-
-    useEffect(() => {
-        // Reset selections for all rows except the last one
-        const newSelectedPlayers = selectedPlayers.slice(0, rowCount);
-        setSelectedPlayers(newSelectedPlayers);
-    }, [rowCount]);
-
-    const fetchPlayers = async () => {
-        try {
-            const response = await axios.get('http://127.0.0.1:8000/api/players');
-            console.log('API Response:', response);
-            setPlayers(response.data);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
-
-    const handlePlayerSelection = (rowIndex, playerId) => {
-        const updatedSelectedPlayers = [...selectedPlayers];
-        updatedSelectedPlayers[rowIndex] = playerId;
-        console.log('Selected Players:', updatedSelectedPlayers); // Debugging output
-        setSelectedPlayers(updatedSelectedPlayers);
-    };
+    const [players, setPlayers] = useState([]);
+    const [rowCount, setRowCount] = useState(1); // for number of rows
+    const [selectedPlayerIds, setSelectedPlayerIds] = useState([]); // for selected players
+    const [scores, setScores] = useState(Array.from({ length: 8 }, () => Array.from({ length: 13 }, () => 0)));
 
     const addRow = () => {
         if (rowCount < 8) {
@@ -44,99 +15,91 @@ const AddGamePage = () => {
         }
     };
 
-    // Filter available players based on selections
-    const availablePlayers = players.filter((p) => !selectedPlayers.includes(p.id));
-    console.log('Available Players:', availablePlayers); // Debugging output
+    useEffect(() => {
+        fetchPlayers();
+    }, []);
 
-     // Function to handle score changes in a specific cell
-     const handleScoreChange = (rowIndex, colIndex, value) => {
+    const fetchPlayers = async () => {
+        try {
+            const response = await axios.get('http://127.0.0.1:8000/api/players');
+            setPlayers(response.data);
+            setAvailablePlayers(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const handlePlayerSelection = (rowIndex, playerId) => {
+        const updatedSelectedPlayerIds = [...selectedPlayerIds];
+        updatedSelectedPlayerIds[rowIndex] = playerId;
+        setSelectedPlayerIds(updatedSelectedPlayerIds);
+    };
+
+    const handleScoreChange = (rowIndex, colIndex, value) => {
         const updatedScores = [...scores];
         updatedScores[rowIndex][colIndex] = parseFloat(value) || 0; // Convert value to a number
         setScores(updatedScores);
     };
 
-    // Function to calculate the total for a specific row
     const calculateRowTotal = (rowIndex) => {
         const rowScores = scores[rowIndex];
         const rowTotal = rowScores.reduce((total, score) => total + score, 0);
         return rowTotal;
     };
 
-
     return (
-        <>
-            <div className="center-container">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Player</th>
-                            <th>12</th>
-                            <th>11</th>
-                            <th>10</th>
-                            <th>9</th>
-                            <th>8</th>
-                            <th>7</th>
-                            <th>6</th>
-                            <th>5</th>
-                            <th>4</th>
-                            <th>3</th>
-                            <th>2</th>
-                            <th>1</th>
-                            <th>0</th>
-                            <th>Total</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {Array.from({ length: rowCount }).map((_, rowIndex) => (
-                            <tr key={rowIndex}>
-                                <td>
-                                    <select
-                                        onChange={(e) => handlePlayerSelection(rowIndex, e.target.value)}
-                                        value={selectedPlayers[rowIndex] || ''}
-                                    >
-                                        <option value="">Select a player</option>
-                                        {players.map((p) => (
-                                            <option key={p.id} value={p.id}>
-                                                {`${p.first_name} ${p.last_name}`}
-                                            </option>
-                                        ))}
-                                    </select>
-                                </td>
-                                {/* Add number input boxes for scores */}
-                                {scores[rowIndex].map((score, colIndex) => (
-                                    <td key={colIndex}>
-                                        <input
-                                            type="number"
-                                            className="small-input"
-                                            value={score}
-                                            onChange={(e) => handleScoreChange(rowIndex, colIndex, e.target.value)}
-                                            onBlur={() => { /* You can add any additional logic here */ }}
-                                        />
-                                    </td>
-                                ))}
-                                
-                                {/* Add more columns for scores here */}
-                                <td>{calculateRowTotal(rowIndex)}</td>
-                            </tr>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Player</th>
+                        {[...Array(13).keys()].map(i => (
+                            <th key={i}>{12 - i}</th>
                         ))}
-                        <tr>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {[...Array(rowCount).keys()].map(rowIndex => (
+                        <tr key={rowIndex}>
                             <td>
-                                {rowCount < 8 && (
-                                    <button onClick={addRow}>
-                                        Add Row
-                                    </button>
-                                )}
+                            <select onChange={(e) => handlePlayerSelection(rowIndex, e.target.value)}>
+                                    <option value="">Select a player</option>
+                                    {players.map((p) => (
+                                        <option key={p.id} value={p.id}>
+                                            {`${p.first_name} ${p.last_name}`}
+                                        </option>
+                                    ))}
+                                </select>
                             </td>
+                            {[...Array(13).keys()].map(colIndex => (
+                                <td key={colIndex}>
+                                <input
+                                    type="number"
+                                    className="small-input"
+                                    value={scores[rowIndex][colIndex]}
+                                    onChange={(e) => handleScoreChange(rowIndex, colIndex, e.target.value)}
+                                    inputmode="numeric"
+                                    min={0}
+                                />
+                            </td>
+                            ))}
+                            <td>{calculateRowTotal(rowIndex)}</td>
                         </tr>
-                    </tbody>
-                </table>
-
-                <button>
-                    Save Game
-                </button>
-            </div>
-        </>
+                    ))}
+                    <tr>
+                        <td colSpan="15">
+                            {rowCount < 8 && (
+                                <button onClick={addRow}>
+                                    Add Row
+                                </button>
+                            )}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     );
-}
+};
 
 export default AddGamePage;
---
+++ mexicantrain/frontend/src/components/saveGames.js
@@ -0,0 +1,152 @@
+import React, { useState, useEffect, useMemo } from "react";
+import axios from 'axios';
+import "/Users/sophiahall/Documents/TrenDB/mexicantrain/frontend/static/css/gameEntry.css";
+
+
+const AddGamePage = () => {
+
+    const [players, setPlayers] = useState([]); // state to store players
+    const [selectedPlayers, setSelectedPlayers] = useState([]); // state to store players which have been selected
+    const [rowCount, setRowCount] = useState(1); // state to store number of rows in the table
+    const [scores, setScores] = useState(Array.from({ length: rowCount }, () => Array(14).fill(0))); // State to store scores
+
+
+    useEffect(() => {
+        fetchPlayers();
+    }, []);
+
+    useEffect(() => {
+        // Reset selections for all rows except the last one
+        const newSelectedPlayers = selectedPlayers.slice(0, rowCount);
+        setSelectedPlayers(newSelectedPlayers);
+
+        // Update scores whenever rowCount changes
+        setScores(prevScores => {
+            const newScores = [...prevScores];
+            newScores.push(Array(14).fill(0)); // Initialize scores for the new row
+            return newScores;
+        });
+    }, [rowCount]);
+    
+    // Fetches the list of players from the api
+    const fetchPlayers = async () => {
+        try {
+            const response = await axios.get('http://127.0.0.1:8000/api/players');
+            console.log('API Response:', response);
+            setPlayers(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    // Updates the list of selected players when a player is selected
+    const handlePlayerSelection = (rowIndex, playerId) => {
+        const updatedSelectedPlayers = [...selectedPlayers];
+        updatedSelectedPlayers[rowIndex] = playerId;
+        console.log('Selected Players:', updatedSelectedPlayers); // Debugging output
+        setSelectedPlayers(updatedSelectedPlayers);
+    };
+
+    // Adds a row to the table
+    const addRow = () => {
+        if (rowCount < 8) {
+            setRowCount(rowCount + 1);
+        }
+    };
+
+    // Filter available players based on selections
+    const availablePlayers = players.filter((p) => !selectedPlayers.includes(p.id));
+    console.log('Available Players:', availablePlayers); // Debugging output
+
+     // Function to handle score changes in a specific cell
+     const handleScoreChange = (rowIndex, colIndex, value) => {
+        const updatedScores = [...scores];
+        updatedScores[rowIndex][colIndex] = parseFloat(value) || 0; // Convert value to a number
+        setScores(updatedScores);
+    };
+
+    // Function to calculate the total for a specific row
+    const calculateRowTotal = (rowIndex) => {
+        const rowScores = scores[rowIndex];
+        const rowTotal = rowScores.reduce((total, score) => total + score, 0);
+        return rowTotal;
+    };
+
+
+    return (
+        <>
+            <div className="center-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Player</th>
+                            <th>12</th>
+                            <th>11</th>
+                            <th>10</th>
+                            <th>9</th>
+                            <th>8</th>
+                            <th>7</th>
+                            <th>6</th>
+                            <th>5</th>
+                            <th>4</th>
+                            <th>3</th>
+                            <th>2</th>
+                            <th>1</th>
+                            <th>0</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Array.from({ length: rowCount }).map((_, rowIndex) => (
+                            <tr key={rowIndex}>
+                                <td>
+                                    <select
+                                        onChange={(e) => handlePlayerSelection(rowIndex, e.target.value)}
+                                        value={selectedPlayers[rowIndex] || ''}
+                                    >
+                                        <option value="">Select a player</option>
+                                        {players.map((p) => (
+                                            <option key={p.id} value={p.id}>
+                                                {`${p.first_name} ${p.last_name}`}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </td>
+                                {/* Add number input boxes for scores */}
+                                {scores[rowIndex].map((score, colIndex) => (
+                                    <td key={colIndex}>
+                                        <input
+                                            type="number"
+                                            className="small-input"
+                                            value={score}
+                                            onChange={(e) => handleScoreChange(rowIndex, colIndex, e.target.value)}
+                                            onBlur={() => { /* You can add any additional logic here */ }}
+                                        /> 
+                                    </td>
+                                ))} 
+                                
+                                {/* Add more columns for scores here */}
+                                <td>{calculateRowTotal(rowIndex)}</td>
+                            </tr>
+                        ))}
+                        <tr>
+                            <td>
+                                {rowCount < 8 && (
+                                    <button onClick={addRow}>
+                                        Add Row
+                                    </button>
+                                )}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <button>
+                    Save Game
+                </button>
+            </div>
+        </>
+    );
+}
+
+export default AddGamePage;
